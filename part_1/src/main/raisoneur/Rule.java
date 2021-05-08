6d5949890b13dc186551b55214ba31c7d477f9da
aymmenkhs <aymenkhouas@gmail.com>
2021-05-08 08:25:56 +0100

    Rule init

--- part_1/src/main/raisoneur/Rule.java
+++ part_1/src/main/raisoneur/Rule.java
@@ -1,4 +1,41 @@
 package main.raisoneur;
 
-public class Rule {
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Rule implements Iterable<Condition>{
+
+    private ArrayList<Condition> conditions;
+    private String rulename, resultats, variableResultat;
+
+    public Rule(String rulename, ArrayList<Condition> conditions, String resultats, String variableResultat) {
+        this.conditions = conditions;
+        this.rulename = rulename;
+        this.resultats = resultats;
+        this.variableResultat = variableResultat;
+    }
+
+    public Rule(String rulename, JSONObject jsonRule){
+        this.rulename = rulename;
+        this.JSONToRule(jsonRule);
+    }
+
+    public String getResultats() {
+        return resultats;
+    }
+
+    public String getVariableResultat() {
+        return variableResultat;
+    }
+
+    public void JSONToRule(JSONObject jsonRule) {
+
+    }
+
+    @Override
+    public Iterator<Condition> iterator() {
+        return conditions.iterator();
+    }
 }
